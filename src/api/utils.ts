e96ea537bd771dc51e18bd68e8079da95f1c1814
uttamkn <uttamkn15@gmail.com>
2024-07-04 23:32:18 +0530

    Add parseString function to utils.ts

--- src/api/utils.ts
+++ src/api/utils.ts
@@ -2,3 +2,12 @@ export const getToken = (): string => {
   const tokenString = localStorage.getItem("token");
   return tokenString || "";
 };
+
+export const parseString = (descriptionString: string): string[] => {
+  const sentences = descriptionString
+    .split(".")
+    .map((sentence) => sentence.trim())
+    .filter((sentence) => sentence !== "");
+
+  return sentences;
+};
