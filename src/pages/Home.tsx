9a03f7a003f22eb0c866e6afc360b655bc9706dd
uttamkn <uttamkn15@gmail.com>
2024-07-04 13:10:19 +0530

    Add WebcamCapture component for capturing photos

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,13 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../context/UserContext";
+import Navbar from "../components/Navbar";
 import toast from "react-hot-toast";
+import WebcamCapture from "../components/WebcamCapture";
 
 const Home: React.FC = () => {
   const { user, loading } = useAuth();
+  const [image, setImage] = useState<string>("");
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -22,21 +25,13 @@ const Home: React.FC = () => {
   }, [loading, user, navigate]);
 
   return (
-    <div
-      style={{ minHeight: "150vh" }}
-      className="flex flex-col items-center justify-center"
-    >
-      <h1 className="text-4xl font-bold mt-10 mb-5">
-        Welcome to the Home Page
-      </h1>
-      <div className="text-lg">
-        {loading ? (
-          <p>Loading...</p>
-        ) : user ? (
-          <p>Hello, {user.username}!</p>
-        ) : (
-          <p>Redirecting...</p>
-        )}
+    <div className="h-screen">
+      <Navbar />
+      <div className="flex">
+        <div className="w-1/3">
+          <WebcamCapture setImage={setImage} image={image} />
+        </div>
+        <div className="w-2/3 bg-slate-500">REPORT</div>
       </div>
     </div>
   );
