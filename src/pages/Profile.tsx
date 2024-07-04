91c260bb3453deb81c38b2c22c410de23f0242ed
sanjay212003 <139911764+sanjay212003@users.noreply.github.com>
2024-07-04 23:05:40 +0530

    update profile

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../context/UserContext";
 import { toast } from "react-hot-toast";
+import Navbar from "../components/Navbar";
 
 const Profile: React.FC = () => {
   const { user, loading } = useAuth();
@@ -19,9 +20,45 @@ const Profile: React.FC = () => {
 
   return (
     <div>
-      <h1>Profile Page</h1>
+      <Navbar />
+    <div className="min-h-screen bg-gray-100 flex flex-col items-center py-10">
+      <div className="w-full max-w-4xl bg-white shadow-md rounded-lg p-4">
+        <div className="flex justify-between items-center mb-6">
+          <h1 className="text-3xl font-bold">SafeSpoon</h1>
+          <div className="flex items-center">
+            <span className="mr-4 font-bold text-2xl ">Hello, {user.username}</span>
+          </div>
+        </div>
+        <div className="flex gap-6">
+          <div className="bg-gray-200 p-6 rounded-lg w-1/2">
+            <h2 className="text-2xl font-semibold mb-4">My Profile</h2>
+            <p className="mb-2">
+              <strong>Name:</strong> {user.username}
+            </p>
+            <p className="mb-2">
+              <strong>Age:</strong> {user.age}
+            </p>
+            <p className="mb-2">
+              <strong>Allergies:</strong> {user.allergies.join(", ")}
+            </p>
+          </div>
+          <div className="w-1/2">
+            <div className="bg-gray-200 p-6 rounded-lg mb-6">
+              <h2 className="text-2xl font-semibold mb-4">Weight</h2>
+              <p>{user.weight} kg</p>
+            </div>
+            <div className="bg-gray-200 p-6 rounded-lg">
+              <h2 className="text-2xl font-semibold mb-4">Diseases</h2>
+              <p>{user.anyDiseases}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
     </div>
   );
 };
+
+
 
 export default Profile;
