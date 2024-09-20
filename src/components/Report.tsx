d5ea6c98ddec5259b40140c034b1c90efa309506
uttamkn <uttamkn15@gmail.com>
2024-09-20 14:38:38 +0530

    Refactor Report component and add ScrollArea component

--- src/components/Report.tsx
+++ src/components/Report.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react";
 import { ReportT } from "../types";
 import { CheckCircle, XCircle } from "lucide-react";
+import { ScrollArea } from "./ui/scroll-area";
 import IngredientCard from "./IngredientCard";
 
 type ReportProps = {
@@ -9,35 +10,48 @@ type ReportProps = {
 
 const Report: FC<ReportProps> = ({ report }) => {
   return (
-    <div className="space-y-6">
-      <div className="flex items-center justify-center space-x-2">
-        {report.is_valid && report.ingredient_risks.length !== 0 ? (
-          report.is_safe ? (
-            <div className="flex items-center space-x-2">
-              <CheckCircle className="h-full w-full text-green-500" />
-              <p className="text-3xl font-semibold text-green-600">
-                Safe to consume
-              </p>
-            </div>
-          ) : (
-            <p className="text-lg font-semibold text-yellow-600">
-              Some ingredients are unsafe.
+    <div className="flex h-full w-full flex-col items-center justify-around">
+      {report.is_valid && report.ingredient_risks.length !== 0 ? (
+        report.is_safe ? (
+          <div className="flex flex-col items-center justify-center">
+            <CheckCircle className="h-32 w-32 text-green-500 md:h-20 md:w-20" />
+            <p className="text-3xl font-semibold text-green-600">
+              Safe to consume
             </p>
-          )
-        ) : (
-          <div className="flex h-full w-full flex-col items-center">
-            <XCircle className="h-full w-full text-red-500" />
-            <p className="text-3xl font-semibold text-red-500">Invalid input</p>
           </div>
-        )}
-      </div>
+        ) : (
+          <p className="text-3xl font-semibold text-yellow-600">
+            Some ingredients are unsafe.
+          </p>
+        )
+      ) : (
+        <div className="flex h-full w-full flex-col items-center justify-center">
+          <XCircle className="h-32 w-32 text-red-500 md:h-20 md:w-20" />
+          <p className="text-3xl font-semibold text-red-600">Invalid input</p>
+        </div>
+      )}
 
       {report.is_valid && (
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3">
-          {report.ingredient_risks.map((risk, index) => (
-            <IngredientCard key={index} risk={risk} />
-          ))}
-        </div>
+        <>
+          <ScrollArea className="h-[300px] overflow-y-auto sm:h-[400px] md:h-[500px] lg:h-[600px]">
+            <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+              {report.ingredient_risks.map((risk, index) => (
+                <IngredientCard key={index} risk={risk} />
+              ))}
+            </div>
+          </ScrollArea>
+
+          <div className="w-full max-w-4xl">
+            <div className="mt-6 pt-4 text-center text-xs text-gray-500 dark:border-zinc-700 dark:text-gray-400 md:text-sm">
+              <p className="italic">
+                Disclaimer: The information provided in the report is based on
+                the data you provided and is not intended as medical advice.
+                Always consult a healthcare provider for dietary
+                recommendations.
+              </p>
+            </div>
+          </div>
+        </>
       )}
     </div>
   );
